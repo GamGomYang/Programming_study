72a8e8b32f8beb266e8552b1376cd3015638642b
GamGomYang <ay6656@naver.com>
2024-11-05 14:02:56 +0900

    1105 modified

---
+++ 기초컴퓨터프로그래밍/실습 코드/10-2.c
@@ -0,0 +1,8 @@
+#include<stdio.h>
+int main(void){
+    int i;
+    for(int i=0; i<5;i++){
+        printf("hello world\n");
+    }
+  return 0;
+}
---
+++ 기초컴퓨터프로그래밍/실습 코드/10-3.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+
+int main(void){
+    for(int i=0; i<5;i++){
+        for(int j=0; j<10;j++){
+            printf("*");
+        }
+        printf("\n");
+    }
+    /*for(int i=1;i<5;i++){
+        for(int j=1;j<=i;j++){
+            printf("*");
+        }
+        printf("\n");
+    }*/
+    return 0;
+
+}
