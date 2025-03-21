3c504287b6a2388d9c1ac9c87adbdb59e78e89d9
GamGomYang <ay6656@naver.com>
2025-03-21 15:56:43 +0900

    03.21.02

--- 객체지향프로그래밍/2-1.cpp
+++ 객체지향프로그래밍/2-1.cpp
@@ -1,21 +1,32 @@
-#include<iostream>
-
+#include <iostream>
 using namespace std;
 
+int main() {
+    int M[3][3] = {{1, 2, 3}, {4, 5, 6}, {7, 8, 9}};
+    int(*ptr)[3];
+    int *p;
+    int **pt;
 
-int main(){
+    ptr = M;
+    cout << ptr << " " << M << endl;
 
-int n = 10;
-char c = 'A';
-double d;
+    cout << (ptr + 1) << " " << (M + 1) << endl;
 
-int *p = &n;
+    cout << *(ptr + 1) << " " << ptr[1] << " "
+         << *(M + 1) << " " << M[1] << endl;
 
+    cout << **(ptr + 1) << " " << **(M + 1) << " "
+         << *M[1] << " " << M[1][0] << endl;
 
-*p = 25;
+    p = M[0];
+    cout << p << " " << M[0] << " " << *M << endl;
 
+    cout << (p + 1) << " " << (M[0] + 1) << " " << (*M + 1) << endl;
+    cout << *(p + 1) << " " << *(M[0] + 1) << " " << *(*M + 1) << endl;
 
-cout << n << *p<<"\n";
+    pt = &p;
+    cout << *pt << " " << p << endl;
+    cout << **pt << " " << *p << endl;
 
+    return 0;
 }
-
