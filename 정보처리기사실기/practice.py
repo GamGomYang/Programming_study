136bb53a097fd6c6d9b2a9df795b3a77da9e0696
GamGomYang <ay6656@naver.com>
2025-10-08 18:22:11 +0900

    10.08 정처기 공부

--- 정보처리기사실기/practice.py
+++ 정보처리기사실기/practice.py
@@ -27,3 +27,61 @@
 # func = lambda x : x*x-3
 # print(func(10))
 
+# TestList= [1,2,3,4,5]
+# TestList= list(map(lambda num : num +100 , TestList))
+# print(TestList)
+
+
+# a = "remember november"
+# b = a[:3] + a[12:16]
+# c = "r and %s" % "STR"
+# print(b+c)
+
+# a,b = 100, 200
+# print(a == b)
+
+# a = 100
+# result =0
+# count =0
+
+# for i in range(1,3):
+#     result = a>>i
+#     print(i, result)
+#     result = result+ i
+#     print(i, result) 
+# print(result)
+
+# class good:
+#     li = ["seoul", "kyeonggi", "inchon", "deajeon", "deagu", "pusan"]
+
+# g = good()
+# str01 = ' '
+# for i in g.li:
+#     str01 = str01 + i[0]
+
+# print(str01)
+
+# lol = [[1,2,3],[4,5],[6,7,8,9]]
+# print(lol[0])
+# print(lol[2][1])
+
+# for sub in lol:
+#     for item in sub:
+#         print(item, end = '')
+#     print()
+
+
+# def fnCalculation(x,y):
+#     result = 0
+#     for i in range(len(x)):
+#         temp = x[i:i+len(y)]
+#         if temp == y:cd
+#             result += 1
+#     return result
+
+# a = "abdcabcabca"
+# pa = "ab"
+# pb = "ca"
+# out = f"ab{fnCalculation(a,pa)}ca{fnCalculation(a,pb)}"
+# print(out)    
+
