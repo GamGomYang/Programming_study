b9d5d9a83c778a108810185dd32683ccf0a3c77a
GamGomYang <ay6656@naver.com>
2025-12-17 17:12:31 +0900

    new branch

--- 정보처리기사실기/TestKorean.java
+++
@@ -1,9 +0,0 @@
-public class TestKorean {
-    public static void main(String[] args) {
-        System.out.println("안녕하세요! VS Code Run Code 테스트입니다.");
-        System.out.println("한국어가 제대로 출력되는지 확인해보세요.");
-        System.out.println("특수문자: !@#$%^&*()");
-        System.out.println("숫자: 1234567890");
-    }
-}
-
