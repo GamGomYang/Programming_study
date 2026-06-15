2ea1441bbdb0a4e3d456921fb62faeca9c0dbbf8
GamGomYang <ay6656@naver.com>
2026-06-15 15:18:22 +0900

    06.15.01

--- 코딩테스트/AA.py
+++ 코딩테스트/AA.py
@@ -1,27 +1,14 @@
-import sys
-sys.stdin = open("input.txt" , "rt")
-def dfs(L):
-    global cnt 
-    if L == m :
-        for j in range(L):
-            print(res[j] , end = ' ')
-        print()
-        cnt +=1
-    else:
-        for i in range(1 , n+1):
-            # 순열은 체크 ch방법으로 구현함
-            if ch[i] == 0:
-                ch[i] = 1
-                res[L] = i
-                dfs(L+1)
-                ch[i] = 0
-    
+def solution(sizes):
 
-if __name__ == "__main__":
-    n, m = map(int,input().split())
-    cnt = 0
-    ch = [0]*(n+1)
-    res = [0] * (m+1)
-    dfs(0)
-    print(cnt)
+    max_w = 0
+    max_h = 0
 
+    for w,h in sizes:
+        big = max(w,h)
+        small = min(w, h)
+
+        max_w = max(max_w , big)
+        max_h = max(max_h, small)
+
+    answer = max_w * max_h
+    return answer
